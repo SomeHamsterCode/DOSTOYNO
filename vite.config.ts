54430fb952e9c283aef6410c66d79cb70cf41fa4
SomeHamsterCode <semenchukeg@gmail.com>
2026-03-05 20:04:36 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Название вашего репозитория - ключ к решению!
-const repoName = 'DOSTOYNO'
-
 export default defineConfig({
-  // Правильный base для GitHub Pages
   base: '/DOSTOYNO/',
-  plugins: [react()], // Уберите viteSingleFile(), если он есть!
+  plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
     emptyOutDir: true,
-    sourcemap: false, // Для чистоты можно отключить
   }
 })
